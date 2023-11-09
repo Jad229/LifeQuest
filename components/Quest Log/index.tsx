c1878ab26444bc463b8f9d6337d2e8ac21ab1a07
Jad229 <76543236+Jad229@users.noreply.github.com>
2023-11-09 13:42:02 -0500

    created quest tile added some styles

--- components/Quest Log/index.tsx
+++ components/Quest Log/index.tsx
@@ -3,9 +3,11 @@ import QuestTile from "./QuestTile";
 function QuestLog() {
   return (
     <section className="text-center flex-col">
-      <h1>Quest log</h1>
-      <div>
+      <h1 className="text-2xl mb-5">Quest log</h1>
+      <div className="flex flex-col mx-auto gap-2 justify-center lg:w-1/2">
         <QuestTile title="First Quest" />
+        <QuestTile title="Second Quest" />
+        <QuestTile title="Third Quest" />
       </div>
     </section>
   );
