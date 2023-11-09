532005f70ad19e4dd6905be71e3746bec807455b
Jad229 <76543236+Jad229@users.noreply.github.com>
2023-11-09 12:43:35 -0500

    created sample home dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <main className="p-5">
+      <div className="flex justify-between items-center">
+        <h2 className="font-bold text-xl">Welcome, User</h2>
+        <div>Image goes here</div>
+      </div>
+      <section className="text-center flex-col">
+        <h1>Quest log</h1>
+        <div>
+          <div className="flex justify-between items-center bg-black text-white">
+            <h3>Quest Title</h3>
+            <button className="bg-red-600 p-2">+</button>
+          </div>
+        </div>
+      </section>
+    </main>
   );
 }
