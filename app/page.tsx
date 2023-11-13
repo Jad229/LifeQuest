0e5d506131de17dd73cf6eb84547d456a4c84faf
Jad229 <76543236+Jad229@users.noreply.github.com>
2023-11-13 14:51:19 -0500

    simple task form for dailies

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,17 @@ export default function Home() {
     <main className="p-5 flex flex-col gap-6">
       <div className="flex justify-between items-center">
         <h2 className="font-bold text-xl">Welcome, User</h2>
-        <Image className="rounded-full" src="https://cdn.vectorstock.com/i/preview-1x/08/19/gray-photo-placeholder-icon-design-ui-vector-35850819.jpg" alt="profile picture default" width={75} height={75}/>
+        <Image
+          className="rounded-full"
+          src="https://cdn.vectorstock.com/i/preview-1x/08/19/gray-photo-placeholder-icon-design-ui-vector-35850819.jpg"
+          alt="profile picture default"
+          width={75}
+          height={75}
+        />
       </div>
-      <ExperienceBar/>
-      <QuestLog />
-      <Leaderboard/>
+      <ExperienceBar />
+      <QuestLog title="Dailies" />
+      <Leaderboard />
     </main>
   );
 }
--- components/QuestLog/index.tsx
+++ components/QuestLog/index.tsx
@@ -1,4 +1,9 @@
+"use client";
+import { useState } from "react";
 import QuestTile from "./QuestTile";
+interface QuestLogProps {
+  title: string;
+}
 
 const quests: {
   id: number;
@@ -46,11 +51,27 @@ const quests: {
   },
 ];
 
-function QuestLog() {
+function QuestLog({ title }: QuestLogProps): JSX.Element {
+  const [isFormOpen, setIsFormOpen] = useState(false);
+
+  function openForm() {
+    setIsFormOpen(true);
+  }
+
+  function closeForm() {
+    setIsFormOpen(false);
+  }
   return (
     <section className="text-center">
-      <h1 className="text-2xl mb-5">Quest log</h1>
+      <h1 className="text-2xl mb-5">{title}</h1>
       <div className="flex flex-col mx-auto justify-center lg:w-1/2 border border-neutral-500 rounded-lg shadow-xl">
+        <button
+          onClick={openForm}
+          className="bg-amber-400 w-1/3 p-2 rounded-sm text-black font-semibold self-end mr-3 mt-3"
+        >
+          + Add new task
+        </button>
+        <hr className="my-3 opacity-30" />
         {quests.map((quest) => (
           <QuestTile
             title={quest.title}
@@ -59,6 +80,43 @@ function QuestLog() {
           />
         ))}
       </div>
+
+      {isFormOpen && (
+        <div className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50">
+          <form className="flex flex-col gap-2 px-12 py-6 mx-auto w-3/4 md:w-1/2 lg:w-1/3 bg-white text-black">
+            <div className="form-group ">
+              <label>Title</label>
+              <input type="text" placeholder="Add a title" />
+            </div>
+            <div className="form-group ">
+              <label>Description</label>
+              <textarea placeholder="Add a title" />
+            </div>
+            <div className="form-group ">
+              <label>Difficulty</label>
+              <select className="text-black" name="difficulty" id="difficulty">
+                <option>Hard</option>
+                <option>Medium</option>
+                <option>Easy</option>
+              </select>
+            </div>
+            <div className="form-group ">
+              <label>Tags</label>
+              <select className="text-black" name="tag" id="tag">
+                <option>Fitness & Health</option>
+                <option>Work</option>
+                <option>Chores</option>
+                <option value="">Personal</option>
+                <option value="">Social</option>
+                <option value="">Creativity</option>
+              </select>
+            </div>
+            <button onClick={closeForm} className="bg-red-600 p-2">
+              Close
+            </button>
+          </form>
+        </div>
+      )}
     </section>
   );
 }
