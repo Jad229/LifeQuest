5c658b5eaaae8951faab4bdaeb875c1527c93c89
Jad229 <jad229@njit.edu>
2023-11-13 21:47:38 -0500

    data fetching from homepage

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-       domains: ["cdn.vectorstock.com"]
+       remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "cdn.vectorstock.com",
+            } 
+       ]
     }
 }
 
