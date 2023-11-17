dfb33d6aa979fa302cf1f3a31a9ee869d34e4d28
Jad229 <jad229@njit.edu>
2023-11-17 14:13:02 -0500

    fixed test and node version in yaml

--- __tests__/sanity.test.js
+++ __tests__/sanity.test.js
@@ -5,7 +5,7 @@ test('adds 1 + 2 to equal 3', () => {
   });
   
   test('multiplication test', () => {
-    expect(2 - 3).toBe(6);
+    expect(2 * 3).toBe(6);
   });
   
   test('string test', () => {
