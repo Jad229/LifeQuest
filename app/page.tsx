c164542b1429d2fdc919729fe4d0189ba235bf33
Jad229 <jad229@njit.edu>
2023-11-18 23:29:22 -0500

    placeholder

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default async function Home(){
   return (
     <main className="max-w-6xl mx-auto p-5 flex flex-col gap-6">
       <div className="flex justify-between items-center">
-        <h2 className="font-bold text-xl">Welcome, User</h2>
+        <h2 className="font-bold text-xl">Welcome, Username</h2>
         <Image
           className="rounded-full"
           src="https://cdn.vectorstock.com/i/preview-1x/08/19/gray-photo-placeholder-icon-design-ui-vector-35850819.jpg"
