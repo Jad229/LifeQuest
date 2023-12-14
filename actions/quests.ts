a98d565d38a3b738959b9194bc1b1c1914bd3bea
Jad229 <jad229@njit.edu>
2023-12-13 23:48:51 -0500

    Refactor createQuestAction function to handle errors and provide better error messages

--- actions/quests.ts
+++ actions/quests.ts
@@ -19,30 +19,44 @@ type Session = {
 };
 
 export async function createQuestAction(data: FormData) {
-  const session: Session | null = await getServerSession(authOptions);
-  const userId: string = session?.user?.id as string;
-  const user: User | null = await getUser(userId);
-  const userLevel: number = user?.level as number;
+  try {
+    const session: Session | null = await getServerSession(authOptions);
+    const userId: string = session?.user?.id ?? "";
+    if (!userId) throw new Error("User ID not found");
 
-  const title: string = data.get("title") as string;
-  const description: string = data.get("description") as string;
-  const difficulty: string = data.get("difficulty") as string;
-  const skill: string = data.get("skill") as string;
-  const category: string = data.get("category") as string;
+    const user: User | null = await getUser(userId);
+    if (!user) throw new Error("User not found");
 
-  if (!title || !description || !difficulty || !skill || !category) return;
+    const userLevel: number = user.level ?? 1; // Set a default level if not available
 
-  const expGain: number = calculateExpGain(difficulty, userLevel);
+    const title: string = data.get("title") ?? "";
+    const description: string = data.get("description") ?? "";
+    const difficulty: string = data.get("difficulty") ?? "";
+    const skill: string = data.get("skill") ?? "";
+    const category: string = data.get("category") ?? "";
 
-  const quest: Quest = {
-    userId,
-    title,
-    description,
-    difficulty,
-    skill,
-    category,
-    expGain,
-  };
-  await createQuest(quest);
-  revalidatePath("/");
+    if (!title || !description || !difficulty || !skill || !category) {
+      throw new Error("Incomplete quest data");
+    }
+
+    const expGain: number = calculateExpGain(difficulty, userLevel);
+
+    const quest: Quest = {
+      userId,
+      title,
+      description,
+      difficulty,
+      skill,
+      category,
+      expGain,
+    };
+
+    await createQuest(quest);
+    revalidatePath("/");
+  } catch (error) {
+    // Handle the error, e.g., log it or show a user-friendly message
+    console.error("Error creating quest:", error.message);
+    // Optionally rethrow the error for higher level handling
+    throw error;
+  }
 }
