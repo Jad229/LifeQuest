fb50dd6e2ad42d48167ffbfacd87a63478695690
Jad229 <jad229@njit.edu>
2024-01-20 15:21:59 -0500

    added a heading to landing page

--- features/Authentication/Unauthorized.tsx
+++ features/Authentication/Unauthorized.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 export default function Unauthorized() {
   return (
     <div className="w-full h-full flex items-center justify-center">
+      <h1>Welcome to LifeQuest</h1>
       <Card>
         <CardHeader>
           <CardTitle>Please Sign In.</CardTitle>
