5777fabbb818ffde8bbbacbd7321d08eea11bd45
Jad229 <jad229@njit.edu>
2024-01-27 15:21:57 -0500

    Update SubmitButton appearance and behavior

--- components/forms/SubmitButton.tsx
+++ components/forms/SubmitButton.tsx
@@ -7,9 +7,13 @@ export default function SubmitButton() {
     <Button
       disabled={pending}
       type="submit"
-      className="disabled:cursor-not-allowed mt-5 btn text-black bg-green-500"
+      className="disabled:cursor-not-allowed mt-5 btn w-11/12 mx-auto text-black bg-green-500 transition-all focus:scale-110 hover:scale-110 active:scale-105 hover:bg-green-600 active:bg-green-400"
     >
-      {pending ? "Submitting..." : "Submit"}
+      {pending ? (
+        <div className="h-5 w-5 animate-spin rounded-full border-b-2 border-white"></div>
+      ) : (
+        "Submit"
+      )}
     </Button>
   );
 }
